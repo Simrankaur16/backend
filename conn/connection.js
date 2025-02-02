302e3c2a01ed819cb7b4566931f0a4e94d149687
Simrankaur16 <kau16156@sheridancollege.ca>
2025-02-02 09:11:48 -0500

    Update connection.js

--- conn/connection.js
+++ conn/connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const conn = async() =>{
     try {
-        await mongoose.connect("mongodb+srv://bsa:sim@cluster0.y1rem.mongodb.net/Bookstore");
+        await mongoose.connect("");
         console.log("Connected to DB");
     } catch(error){
         console.log(error);
